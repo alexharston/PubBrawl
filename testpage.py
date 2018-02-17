3009310863613f44dc4ba1cdc06810e00dffb865
Alex Harston <j.harston17@imperial.ac.uk>
2018-02-17 15:04:28 +0000

    added CSS templating and updated gitignore

--- testpage.py
+++ testpage.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def title():
-    return 'Test page'
+    return 'This is where our hack stuff will presumably go. Maps or some shit.'
 
 
 @app.route('/map', methods=['GET', 'POST'])
 def showmap():
+    return "This is where the map should go."
     if request.method == 'POST':
         pass
+    elif request.method == 'GET':
+        return 'This is where the map should be.'
     else:
         pass
