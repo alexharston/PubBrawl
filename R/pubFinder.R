0e6d2d86262a21bfb530ce83fe19ec2356420311
sebSteinhauser <sebastian.steinhauser@crick.ac.uk>
2018-02-17 18:14:59 +0000

    Added python script to call R functions

--- R/pubFinder.R
+++ R/pubFinder.R
@@ -1,36 +1,49 @@
-library(googleway)
-library(geosphere)
-library(dplyr)
-library(tidyr)
+suppressMessages(library(googleway))
+suppressMessages(library(geosphere))
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
+suppressMessages(library(getopt))
+suppressMessages(library(jsonlite))
 
-# Define API key
-key <- ' AIzaSyBaNsBP0XxPQ3Y0V-WMf7fzj9ZSac2nzak '
+#==============================================================================#
+#                             FUNCTIONS
+#==============================================================================#
+findPubs <- function(x.start, y.start, x.end, y.end, api.key) {
+  # Find mindpoint between both locations
+  x.mid <- midPoint(c(x.start, y.start), c(x.end, y.end))[1]
+  y.mid <- midPoint(c(x.start, y.start), c(x.end, y.end))[2]
 
-# Test case
-# Start location
-x.start <- 51.534186
-y.start <- -0.138886
+  # Query goolge API to find all pubs within a radius around
+  pubs.list <- google_places(search_string = 'pub',
+                             location = c(x.mid, y.mid),
+                             radius = 20000,
+                             #rankby = 'distance',
+                             #place_type = c('bar', 'restaurant'),
+                             key = api.key)
 
-# End location
-x.end <- 51.517647
-y.end <- -0.119974
+  # Tidy API search results
+  pubs.df <- data_frame(pub_name = pubs.list$results$name) %>%
+    bind_cols(., pubs.list$results$geometry$location) %>%
+    mutate(rating = pubs.list$results$rating,
+           price_level = pubs.list$results$price_level)
+  return(toJSON(pubs.df))
 
-# Find mindpoint between both locations
-x.mid <- midPoint(c(x.start, y.start), c(x.end, y.end))[1]
-y.mid <- midPoint(c(x.start, y.start), c(x.end, y.end))[2]
+}
 
-# Query goolge API to find all pubs within a radius around
-pubs.list <- google_places(search_string = 'pub',
-                           location = c(x.mid, y.mid),
-                           radius = 20000,
-                           #rankby = 'distance',
-                           #place_type = c('bar', 'restaurant'),
-                           key = key)
+#==============================================================================#
+#                           MAIN
+#==============================================================================#
+## Define API key
+#key <- ' AIzaSyBaNsBP0XxPQ3Y0V-WMf7fzj9ZSac2nzak '
+#
+## Test case
+## Start location
+#x.start <- 51.534186
+#y.start <- -0.138886
+#
+## End location
+#x.end <- 51.517647
+#y.end <- -0.119974
 
-# Tidy API search results
-pubs.df <- data_frame(pub_name = pubs.list$results$name) %>%
-  bind_cols(., pubs.list$results$geometry$location) %>%
-  mutate(rating = pubs.list$results$rating,
-         price_level = pubs.list$results$price_level)
 
-write_json(pubs.df, '/Users/steinhs/privat_projects/HackCity18/pubs.json')
+#write_json(pubs.df, '/Users/steinhs/privat_projects/HackCity18/pubs.json')
---
+++ call_r_function.py
@@ -0,0 +1,20 @@
+from rpy2.robjects.packages import STAP
+
+# Get R function from file
+with open('./R/pubFinder.r', 'r') as f:
+    r_functions = f.read()
+    findPubs = STAP(r_functions, 'findPubs')
+
+#### Test R function
+key = 'AIzaSyBaNsBP0XxPQ3Y0V-WMf7fzj9ZSac2nzak'
+#
+# Start location
+x_start = 51.534186
+y_start = -0.138886
+#
+# End location
+x_end = 51.517647
+y_end = -0.119974
+
+pubs = findPubs.findPubs(x_start, y_start, x_end, y_end, key)
+print pubs
