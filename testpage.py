4899f0a67e548d71110c830bc874f65a1b50b67f
Alex Harston <j.harston17@imperial.ac.uk>
2018-02-17 18:52:24 +0000

    added testing page

--- testpage.py
+++ testpage.py
@@ -37,6 +37,32 @@ def mapview():
     )
     return render_template('map.html', mymap=mymap)
 
+@app.route('/test/')
+def testview():
+
+    GOOGLE_MAPS_API_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+    params = {
+    'address': 'Oxford Street, London, United Kingdom',
+    'sensor': 'false',
+    'region': 'uk'
+    }
+
+    # Do the request and get the response data
+    req = requests.get(GOOGLE_MAPS_API_URL, auth=( 'AIzaSyCidFr8iYBBvsMPbal07w_PUuN6Xa0uEOA'))
+    res = req.json()
+
+    # Use the first result
+    result = res['results'][0]
+
+    geodata = dict()
+    geodata['lat'] = result['geometry']['location']['lat']
+    geodata['lng'] = result['geometry']['location']['lng']
+    geodata['address'] = result['formatted_address']
+
+    print('{address}. (lat, lng) = ({lat}, {lng})'.format(**geodata))
+    # 221B Baker Street, London, Greater London NW1 6XE, UK. (lat, lng) = (51.5237038, -0.1585531)
+    return render_template('test.html')
+   
 if __name__ == "__main__":
     app.debug=True
     app.run(host='0.0.0.0', port=5000)
