827eec11dc00cc7433d1082a2446c86205669773
Alex Harston <j.harston17@imperial.ac.uk>
2018-02-17 16:35:52 +0000

    adding maps functionality

--- testpage.py
+++ testpage.py
@@ -1,18 +1,40 @@
 from flask import Flask
 from flask import request
+from flask_googlemaps import GoogleMaps
 app = Flask(__name__)
 
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyCidFr8iYBBvsMPbal07w_PUuN6Xa0uEOA"
+
+GoogleMaps(app)
+
 @app.route('/')
-def title():
-    return homepage.html
+def mapview():
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('example.html', mymap=mymap, sndmap=sndmap)
 
-
-@app.route('/map', methods=['GET', 'POST'])
-def showmap():
-    return "This is where the map should go."
-    if request.method == 'POST':
-        pass
-    elif request.method == 'GET':
-        return 'This is where the map should be.'
-    else:
-        pass
+if __name__ == "__main__":
+    app.run(debug=True)
