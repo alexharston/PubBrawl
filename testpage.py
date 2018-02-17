4284f195da81d261fa025cd650f06de28f73242a
Alex Harston <j.harston17@imperial.ac.uk>
2018-02-17 18:22:38 +0000

    added templating

--- testpage.py
+++ testpage.py
@@ -1,40 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
+from flask import json
+from flask import jsonify
 from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 app = Flask(__name__)
 
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyCidFr8iYBBvsMPbal07w_PUuN6Xa0uEOA"
-
-GoogleMaps(app)
+GoogleMaps(app, key="AIzaSyCidFr8iYBBvsMPbal07w_PUuN6Xa0uEOA")
 
 @app.route('/')
+def homeview():
+   return render_template('home.html')
+   
+
+@app.route('/map/')
 def mapview():
     mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
+        identifier="mymap",
+        lat=51.5154,
+        lng=-0.1410,
         markers=[
           {
              'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
+             'lat': 51.5154,
+             'lng': -0.1410,
+             'infobox': "<b>London Pub Crawl Generator</b>"
           },
           {
              'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
+             'lat': 51.5154,
+             'lng': -0.1410,
              'infobox': "<b>Hello World from other place</b>"
           }
         ]
     )
-    return render_template('example.html', mymap=mymap, sndmap=sndmap)
+    return render_template('map.html', mymap=mymap)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug=True
+    app.run(host='0.0.0.0', port=5000)
