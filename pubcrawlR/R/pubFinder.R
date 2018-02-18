9af7071fcf182dfecad4f0e8ab31feaceb5c417b
sebSteinhauser <sebastian.steinhauser@crick.ac.uk>
2018-02-18 01:27:10 +0000

    new

--- pubcrawlR/R/pubFinder.R
+++ pubcrawlR/R/pubFinder.R
@@ -1,16 +1,7 @@
-suppressMessages(library(googleway))
-suppressMessages(library(geosphere))
-suppressMessages(library(dplyr))
-suppressMessages(library(tidyr))
-suppressMessages(library(getopt))
-suppressMessages(library(jsonlite))
-suppressMessages(library(httr))
-suppressMessages(library(lubridate))
-
 #==============================================================================#
 #                             FUNCTIONS
 #==============================================================================#
-findPubs <- function(x.start, y.start, x.end, y.end, api.key) {
+findPubs <- function(x.start, y.start, x.end, y.end, key) {
   # Find mindpoint between both locations
   x.mid <- midPoint(c(x.start, y.start), c(x.end, y.end))[1]
   y.mid <- midPoint(c(x.start, y.start), c(x.end, y.end))[2]
@@ -18,10 +9,10 @@ findPubs <- function(x.start, y.start, x.end, y.end, api.key) {
   # Query goolge API to find all pubs within a radius around
   pubs.list <- google_places(search_string = 'pub',
                              location = c(x.mid, y.mid),
-                             radius = 20000,
+                             radius = 500,
                              #rankby = 'distance',
                              #place_type = c('bar', 'restaurant'),
-                             key = api.key)
+                             key = key)
 
   # Tidy API search results
   pubs.df <- data_frame(pub_name = pubs.list$results$name) %>%
@@ -33,28 +24,10 @@ findPubs <- function(x.start, y.start, x.end, y.end, api.key) {
 }
 
 # Query API
-#url <- 'http://whatpub.com/'
-#path <- 'api/1/SearchPubs/location=London' #=json'
-#raw.result <- GET(url = url, path = path)
-#rawToChar(raw.result$content)
-
-
-#==============================================================================#
-#                           MAIN
-#==============================================================================#
-## Define API key
-#key <- ' AIzaSyBaNsBP0XxPQ3Y0V-WMf7fzj9ZSac2nzak '
-#
-## Test case
-## Start location
-#x.start <- 51.534186
-#y.start <- -0.138886
-#
-## End location
-#x.end <- 51.517647
-#y.end <- -0.119974
-#
-#findPubs(x.start, y.start, x.end, y.end, key)
-
-
-#write_json(pubs.df, '/Users/steinhs/privat_projects/HackCity18/pubs.json')
+#Url <- 'http://whatpub.com/'
+#Pub.name <- paste('The London Pub', collapse = '+')
+#Path <- 'search?q=pub.name'
+#Path <- 'api/1/GetPubDetails'
+#Raw.result <- GET(url = url, path = path)
+#Raw.result$status_code
+#RawToChar(raw.result$content)
--- pubcrawlR/ui.R
+++ pubcrawlR/ui.R
@@ -1,16 +1,47 @@
 library(shiny)
-library(googleway)
-library(geosphere)
+library(shinysky)
+
 library(dplyr)
 library(tidyr)
+library(readr)
+
+library(googleway)
+library(geosphere)
 library(getopt)
 library(jsonlite)
 library(httr)
 library(lubridate)
 
+source('./R/pubFinder.R')
+source('./server.R')
+
+# Define API key
+key <- 'AIzaSyCQRLfT4Svbb0PoG9TKH_aPJiRO6FGSh2c'
+set_key(key)
+
 ui <- fluidPage(
+  # Page title
   titlePanel('Hackcity 2018 project - PubCrawlR'),
-  google_mapOutput(outputId = "map", height = "800px")
+  
+  # Google maps 
+  mainPanel(google_mapOutput(outputId = 'map', height = '800px'))
+  
+  # Sidebars
+  #sidebarLayout(
+  #  # Text input 
+  #  #sidebarPanel(selectInput('start', 'Pub crawl start', choices = '', multiple = T)),
+  #  #sidebarPanel(selectizeInput('start', 'Pub crawl start', choices = '')),
+  #  #sidebarPanel(select2Input('start', 'Pub crawl start', choices = 'Select a start point', type = 'input')), 
+  #  #sidebarPanel(textInput('start', 'Pub crawl start', '')),
+  #  #sidebarPanel(textInput.typeahead(id = 'start',
+  #  #                                 placeholder = '', 
+  #  #                                 local = data_frame(description = 'London'),
+  #  #                                 template = HTML("<p class='des'>{{description}}</p>"),
+  #  #                                 tokens = 1, valueKey = 'description')),
+  #  #
+  #  # Google maps 
+  #  mainPanel(google_mapOutput(outputId = 'map', height = '800px'))
+  #)
 )
 
 shinyApp(ui, server)
